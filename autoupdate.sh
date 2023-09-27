793ae8b2d864c51a67755a6310a124579fe8a332
requiem.moe <externalthunder@gmail.com>
2023-09-27 11:45:22 -0500

    Create autoupdate.sh

---
+++ autoupdate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define the username and script path
+USERNAME="luna"
+SCRIPT_PATH="/home/luna/uur.sh"
+
+# Check if the user exists
+if id "$USERNAME" &>/dev/null; then
+    # Add the sudoers entry for the script without requiring a password
+    echo "$USERNAME ALL=(ALL) NOPASSWD: $SCRIPT_PATH" | sudo EDITOR='tee -a' visudo
+
+    # Create a cron job for the script to run every week (adjust timing as needed)
+    (crontab -l -u $USERNAME; echo "0 0 * * 0 $SCRIPT_PATH") | sort - | uniq - | crontab -u $USERNAME -
+
+    # Output a message
+    echo "Sudo privileges added for $USERNAME to run $SCRIPT_PATH."
+    echo "Cron job scheduled to run $SCRIPT_PATH every week."
+else
+    echo "User $USERNAME does not exist."
+fi
