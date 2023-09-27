bee766f7a018e8b03280e46a3c394139dee59fab
requiem.moe <externalthunder@gmail.com>
2023-09-27 11:48:40 -0500

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -6,6 +6,23 @@ SCRIPT_PATH="/home/luna/uur.sh"
 
 # Check if the user exists
 if id "$USERNAME" &>/dev/null; then
+    # Create the uur.sh file with the specified content
+    cat <<EOL > "$SCRIPT_PATH"
+#!/bin/bash
+
+# Update the package list
+sudo apt update
+
+# Upgrade installed packages
+sudo apt upgrade -y
+
+# Restart the system
+sudo reboot
+EOL
+
+    # Make the script executable
+    chmod +x "$SCRIPT_PATH"
+
     # Add the sudoers entry for the script without requiring a password
     echo "$USERNAME ALL=(ALL) NOPASSWD: $SCRIPT_PATH" | sudo EDITOR='tee -a' visudo
 
